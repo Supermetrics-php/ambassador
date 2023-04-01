cc03935be7d9af82fbbd2ee328e92e9442ec02cf
Farshid Boroomand <farshidboroomand@gmail.com>
2023-04-01 19:51:15 +0330

    Redis Implemented Completely

--- index.php
+++ index.php
@@ -3,9 +3,12 @@ require_once 'vendor/autoload.php';
 
 use Supermetrics\Ambassador\Ambassador;
 
-$ambassador = new Ambassador('mysql');
+$ambassador = new Ambassador('redis');
 
-var_dump($ambassador->persist('users', [
-    ['id' => '111', 'name' => 'farshid'],
-    ['id' => '111', 'name' => 'farshid']
-]));
+//var_dump($ambassador->fetchById('users', '60bb0ca5-25d1-43bd-98e5-6a878c00a0d8'));
+//var_dump($ambassador->persist('users', [
+//    ['id' => '60bb0ca5-25d1-43bd-98e5-6a878c00a0d8', 'name' => 'farshid'],
+//    ['id' => '60bb0ca5-25d1-43bd-98e5-6a878c00a0d9', 'name' => 'mary']
+//]));
+
+var_dump($ambassador->fetchAll('users'));
