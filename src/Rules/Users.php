7411eb2b889d4411211a2926e3ef4d56f0978972
Farshid Boroomand <farshidboroomand@gmail.com>
2023-04-01 11:31:25 +0330

    extra validation added

--- src/Rules/Users.php
+++ src/Rules/Users.php
@@ -12,16 +12,42 @@ class Users implements ValidationInterface
      */
     private const ID = 'id';
     private const NAME = 'name';
+
+    /**
+     * @param array $payload
+     *
+     * @return array
+     */
     public function doValidate(array $payload): array
     {
         $errors = [];
 
         foreach ($payload as $key => $item) {
+            /**
+             * ID must be existed.
+             */
             if (!Validator::key(self::ID)->validate($item)) {
                 $errors[$key] = $item;
             }
 
+            /**
+             * UUID will be checked
+             */
+            if (!Validator::uuid()->validate($item[self::ID])) {
+                $errors[$key] = $item;
+            }
+
+            /**
+             * NAME must be existed.
+             */
             if (!Validator::key(self::NAME)->validate($item)) {
+                $errors[$key] = $item;
+            }
+
+            /**
+             * NAME type must be string.
+             */
+            if (!Validator::stringType()->validate($item[self::NAME])) {
                 $errors[$key] = $item;
             }
         }
