26dd1ec2e0123c886011d8ff54af8e4c5c7867ac
Farshid Boroomand <farshidboroomand@gmail.com>
2023-04-01 13:29:32 +0330

    extra message added

--- src/Enums/ErrorMessages.php
+++ src/Enums/ErrorMessages.php
@@ -9,6 +9,6 @@ enum ErrorMessages: string
     use EnumConverterTrait;
     case INVALID_STORAGE = 'Invalid Storage.';
     case INVALID_ENTITY_TYPE = 'Invalid Entity Type.';
-
     case INVALID_DATA = 'Provided Data Is Invalid.';
+    case STORAGE_CONNECTION_FAILED = 'Database Connection Failed.';
 }
