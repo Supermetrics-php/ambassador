b54db00b237a72eb9d04dfc79d79ce4cd584a49a
Farshid Boroomand <farshidboroomand@gmail.com>
2023-04-02 15:09:39 +0330

    MYSQLdriver removed

--- src/Enums/StorageDrivers.php
+++ src/Enums/StorageDrivers.php
@@ -7,7 +7,6 @@ use Supermetrics\Ambassador\Traits\EnumConverterTrait;
 enum StorageDrivers: string
 {
     use EnumConverterTrait;
-    case MYSQL = 'mysql';
     case REDIS = 'redis';
     case MONGO = 'mongo';
     case FILE = 'file';
