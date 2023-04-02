aadeb0f00eff221c0d43edc363c4a99848b4a7f4
Farshid Boroomand <farshidboroomand@gmail.com>
2023-04-02 17:06:36 +0330

    sampleClientUsage file added

---
+++ sampleClientUsage.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'vendor/autoload.php';
+
+use Supermetrics\Ambassador\Ambassador;
+
+//$ambassador = new Ambassador('file');
+$ambassador = new Ambassador('redis');
+
+$result = $ambassador->persist('users', [
+    ['id' => '60bb0ca5-25d1-43bd-98e5-6a878c00a0d8', 'name' => 'farshid'],
+    ['id' => '60bb0ca5-25d1-43bd-98e5-6a878c00a0d9', 'name' => 'mary'],
+    ['id' => '60bb0ca5-25d1-43bd-98e5-6a878c00a0a8', 'name' => 'farshid'],
+]);
+
+//$result = $ambassador->fetchById('users', '60bb0ca5-25d1-43bd-98e5-6a878c00a0d8');
+//$result = $ambassador->fetchAll('users')
+
+var_dump($result);
